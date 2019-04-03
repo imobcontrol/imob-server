c71e9b354c9a886f72627647c077b341ead50dd9
Raphael Santos <raphael.s.cunha@gmail.com>
2019-04-02 22:26:37 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ class App {
             express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
         );
 
-        this.express.use.use(function(req, res, next) {
+        this.express.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
             res.header(
                 "Access-Control-Allow-Methods",
